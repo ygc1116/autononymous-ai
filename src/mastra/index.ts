bb7bda54a6f84bef11366ff1ac6b9a34545981f1
ygc-tomas <yaguchi.yukihiko@tomastc.com>
2025-04-29 14:03:33 +0700

    add cheat sheet

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -2,13 +2,8 @@
 import { autonomousAgent } from './agents';
 import { Mastra } from '@mastra/core/mastra';
 import { createLogger } from '@mastra/core/logger';
-<<<<<<< HEAD
-
-import { weatherAgent } from './agents';
-=======
 import { reactComponentWorkflow } from './workflows';
 
->>>>>>> ab1c25df57fc773247ca47bbe2a4d7ce01759395
 
 export const mastra = new Mastra({
   workflows: { reactComponentWorkflow },
